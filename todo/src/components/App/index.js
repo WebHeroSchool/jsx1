49b3fbbf225e17ddb9f78e82b25a31b71d879bb0
Artem Mezentsev <artemamway88@gmail.com>
2020-07-11 13:57:14 +0700

    переименовал файлы в index.js и добавил изменения

--- todo/src/components/App/index.js
+++ todo/src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ItemList from '../ItemList/ItemList';
-import InputItem from '../InputItem/InputItem';
-import Footer from '../Footer/Footer';
+import ItemList from '../ItemList';
+import InputItem from '../InputItem';
+import Footer from '../Footer';
 
 const header = (<h1>Заметки:</h1>);
 const App = () => (<div>
