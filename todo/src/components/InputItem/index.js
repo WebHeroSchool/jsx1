248936034a95c0f040e9a048c33d0efbb213711c
Artem Mezentsev <artemamway88@gmail.com>
2020-07-14 17:32:57 +0700

    Добавлен инпут-компонент

--- todo/src/components/InputItem/index.js
+++ todo/src/components/InputItem/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>InputItem</div>);
+const InputItem = () => (<div>
+  <TextField
+    id="filled-basic"
+    label="Filled"
+    variant="filled"
+    size="small"
+  />
+</div>);
 
 export default InputItem;
