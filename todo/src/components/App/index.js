e4f0bb12fa25501f3482ca7044dd43357f9b22ca
Artem Mezentsev <artemamway88@gmail.com>
2020-07-15 14:44:19 +0700

    Изменение компонента-функции на компонент-класс

--- todo/src/components/App/index.js
+++ todo/src/components/App/index.js
@@ -6,28 +6,30 @@ import styles from './App.module.css';
 
 const todoItem = 'Написать приложение';
 const header = (<h1 className = {styles.header}>Заметки:</h1>);
-const App = () => {
-  const items = [
-    {
-      value: todoItem,
-      isDone: false,
-    },
-    {
-      value: 'Выполнить 3-5 заданий в школе',
-      isDone: true,
-    },
-    {
-      value: 'Похвалить себя',
-      isDone: true,
-    }
-  ];
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: todoItem,
+        isDone: false,
+      },
+      {
+        value: 'Выполнить 3-5 заданий в школе',
+        isDone: true,
+      },
+      {
+        value: 'Похвалить себя',
+        isDone: true,
+      }
+    ];
 
-  return (<div className = {styles.body}>
-    {header}
-    <InputItem />
-    <ItemList items = {items} />
-    <Footer count = {3} />
-  </div>);
-}
+    return (<div className = {styles.body}>
+      {header}
+      <InputItem />
+      <ItemList items = {items} />
+      <Footer count = {3} />
+    </div>);
+  }
+};
 
 export default App;
