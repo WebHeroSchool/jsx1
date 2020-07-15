336b6feb98d3179050e53fa28b8ab5835ad6b024
Artem Mezentsev <artemamway88@gmail.com>
2020-07-15 14:16:13 +0700

    подключены несколько модулей, добавлены стили

--- todo/src/components/ItemList/index.js
+++ todo/src/components/ItemList/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Item from '../Item';
+import Checkbox from '@material-ui/core/Checkbox';
+import styles from './ItemList.module.css';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 
 const ItemList = ({ items }) => (<ul>
-  {items.map(item => (<li key = {item.value}>
-      <Item value = {item.value} isDone = {item.isDone} />
+  {items.map(item => (<li key = {item.value} className = {styles.list}>
+      <Checkbox />
+      <Item
+        value = {item.value}
+        isDone = {item.isDone}
+      />
+      <div className = {styles.icons}>
+        <DeleteForeverOutlinedIcon />
+      </div>
   </li>))}
+
 </ul>);
 
 export default ItemList;
