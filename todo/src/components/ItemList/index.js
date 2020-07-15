195a82e68de9b33a0610484f6ad84169fbf538b7
Artem Mezentsev <artemamway88@gmail.com>
2020-07-15 16:41:53 +0700

    добавлена обработка клика по чекбоксу

--- todo/src/components/ItemList/index.js
+++ todo/src/components/ItemList/index.js
@@ -3,14 +3,21 @@ import Item from '../Item';
 import Checkbox from '@material-ui/core/Checkbox';
 import styles from './ItemList.module.css';
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const ItemList = ({ items }) => (<ul>
+const ItemList = ({ items, onClickDone, isDone }) => (<ul>
   {items.map(item => (<li key = {item.value} className = {styles.list}>
-      <Checkbox />
-      <Item
+    <FormControlLabel
+      control = {  
+        <Checkbox 
+          onClick = {() => onClickDone(item.isDone)}
+        />
+      }
+      label = <Item
         value = {item.value}
         isDone = {item.isDone}
       />
+    />
       <div className = {styles.icons}>
         <DeleteForeverOutlinedIcon />
       </div>
