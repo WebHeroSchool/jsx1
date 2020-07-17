870baa4068fb08bab1ed98985699325d79ab2c35
Artem Mezentsev <artemamway88@gmail.com>
2020-07-17 15:22:06 +0700

    Добавлены изменения

--- todo/src/components/ItemList/index.js
+++ todo/src/components/ItemList/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import Item from '../Item';
-import Checkbox from '@material-ui/core/Checkbox';
 import styles from './ItemList.module.css';
-import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const ItemList = ({ items, onClickDone, isDone }) => (<ul>
+const ItemList = ({ items, onClickDone }) => (<ul>
   {items.map(item => (<li key = {item.value} className = {styles.list}>
-    <FormControlLabel
-      control = {  
-        <Checkbox 
-          onClick = {() => onClickDone(item.isDone)}
-        />
-      }
-      label = <Item
-        value = {item.value}
-        isDone = {item.isDone}
-      />
+    <Item
+      value = {item.value}
+      isDone = {item.isDone}
     />
-      <div className = {styles.icons}>
-        <DeleteForeverOutlinedIcon />
-      </div>
   </li>))}
-
 </ul>);
 
 export default ItemList;
