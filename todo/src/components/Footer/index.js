e03f28b2b8f978cdf951e9a4bcbfa9f801392f8f
Artem Mezentsev <artemamway88@gmail.com>
2020-07-30 18:21:42 +0700

    небольшие изменения в Footer

--- todo/src/components/Footer/index.js
+++ todo/src/components/Footer/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Tab from '@material-ui/core/Tab';
 import styles from './Footer.module.css';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
 
 const Footer = ({ count }) => (<div>
     <div className = {styles.tab}>
+      <Badge color="secondary" badgeContent={count}>
+        <MailIcon />
+      </Badge>
       <Tab label="All" />
       <Tab label="Active" />
       <Tab label="Completed" />
-    </div>
-    <div className = {styles.block}>
-      <span className = {styles.count}>{count} items left</span>
-      <span className = {styles.delete}>Clear completed</span>
     </div>
 </div>);
 
